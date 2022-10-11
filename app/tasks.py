ba197da2c041ef33b39d9682e10c1455ac2abe17
Michael <mpgg2000@gmail.com>
2022-10-11 14:30:04 +0300

    phones

--- app/tasks.py
+++ app/tasks.py
@@ -39,6 +39,7 @@ def dispatch(lead_id: int, previous_status=None):
     status = StatusMatch.get_status(previous_status, data.status.id)
     if status is None:
         return
+    hook_logger.warning(contact, contact.phone)
     py_data = BoundHook(
         id=data.id,
         status=status,
