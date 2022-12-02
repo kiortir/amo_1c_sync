9a55f40919a07dbb41973695a4558962aee4d711
Michael <mpgg2000@gmail.com>
2022-12-02 19:57:15 +0300

    y

--- app/statuses.py
+++ app/statuses.py
@@ -14,6 +14,9 @@ class StatusMap:
     delete = {
         "Закрыто и не реализовано"
     }
+    ignore = {
+        'Выезд'
+    }
 
 
 class Status:
@@ -25,7 +28,6 @@ class Status:
         self.name = name
         self.pipeline_id = pipeline_id
         self.statuses[id] = self
-
 
     @classmethod
     def register(cls, id: int, name: str, pipeline_id: int):
@@ -45,7 +47,7 @@ class Condition:
         return self.__class__.__name__
 
     def get_amo_status(self, lead_status: int):
-        print('статусы',lead_status, Status.statuses)
+        print('статусы', lead_status, Status.statuses)
         status = Status.statuses.get(lead_status)
         return status
 
@@ -59,7 +61,9 @@ class create_or_update_booking(Condition):
         status = self.get_amo_status(lead_status)
         if not status:
             return False
-        print(status.name, StatusMap.booking, _1c_status)
+        if status.name in StatusMap.ignore:
+            return
+
         return status.name in StatusMap.booking and _1c_status != 'stay'
 
 
@@ -69,6 +73,8 @@ class create_or_update_stay(Condition):
         status = self.get_amo_status(lead_status)
         if not status:
             return False
+        if status.name in StatusMap.ignore:
+            return
         return status.name in StatusMap.stay
 
 
@@ -78,6 +84,8 @@ class delete_booking(Condition):
         status = self.get_amo_status(lead_status)
         if not status:
             return False
+        if status.name in StatusMap.ignore:
+            return
         return _1c_status == 'booking' \
             and (status.name not in StatusMap.booking
                  and status.name not in StatusMap.stay) \
@@ -90,6 +98,8 @@ class delete_stay(Condition):
         status = self.get_amo_status(lead_status)
         if not status:
             return False
+        if status.name in StatusMap.ignore:
+            return
         return status.name not in StatusMap.stay \
             and _1c_status == 'stay'
 
@@ -100,6 +110,8 @@ class delete_all(Condition):
         status = self.get_amo_status(lead_status)
         if not status:
             return False
+        if status.name in StatusMap.ignore:
+            return
         return _1c_status == 'stay' \
             and status.name in StatusMap.delete
 
@@ -112,5 +124,5 @@ def match_status(lead_status: int, _1c_status):
     for status_entry in statuses:
         print(status_entry)
         m = status_entry.match(lead_status, _1c_status)
-        if m: 
+        if m:
             return status_entry.status_name
