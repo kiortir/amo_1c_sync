089f5f090fe5e6bdb70d8ac67ed405a97d88a6df
kiortir <mpgg2000@gmail.com>
2023-06-14 11:18:46 +0300

    prefix fix

--- src/logs.py
+++ src/logs.py
@@ -8,6 +8,9 @@ __all__ = ["applogger", "amologger", "_1clogger"]
 
 handler = AsyncStreamHandler(stream=sys.stdout)
 
-applogger = Logger(name="app", handlers=[handler])
-amologger = Logger(name="amo", handlers=[handler])
-_1clogger = Logger(name="1c", handlers=[handler])
+applogger = Logger(name="app")
+applogger.add_handler(handler)
+amologger = Logger(name="amo")
+amologger.add_handler(handler)
+_1clogger = Logger(name="1c")
+_1clogger.add_handler(handler)
